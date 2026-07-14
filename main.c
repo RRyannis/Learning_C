b91fcebcf39342ded4218c51cfb454c15773eb1f
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-07-14 13:02:41 +0300

    testing

--- main.c
+++ main.c
@@ -2,17 +2,10 @@
 
 int main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
-    lower = 0;   /* lower limit of
-      temperature scale */
-    upper = 300; /* upper limit */
-    step = 20;   /* step size */
-    fahr = lower;
-    while (fahr <= upper)
-    {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-}
+int fahr;
+for (fahr = 0; fahr <= 300; fahr =
+fahr + 20)
+printf("%3d %6.1f\n", fahr,
+(5.0/9.0)*(fahr-32));
+
+return 0;}
