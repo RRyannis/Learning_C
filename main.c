fb464741718fea2a28ec002a61e3b16faea58de1
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-07-18 17:20:14 +0300

    eh

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-int power(int m, int n);
-/* test power function */
-main()
+/* count digits, white space, others */
+int main()
 {
-int i;
+int c, i, nwhite, nother;
+int ndigit[10];
+nwhite = nother = 0;
 for (i = 0; i < 10; ++i)
-printf("%d %d %d\n", i,
-power(2,i), power(-3,i));
+ndigit[i] = 0;
+while ((c = getchar()) != EOF)
+if (c >= '0' && c <= '9')
+++ndigit[c-'0'];
+else if (c == ' ' || c == '\n' ||
+c == '\t')
+++nwhite;
+else
+++nother;
+printf("digits =");
+for (i = 0; i < 10; ++i)
+printf(" %d", ndigit[i]);
+printf(", white space = %d, other =%d\n",nwhite, nother);
 return 0;
 }
-/* power: raise base to n-th power; n
->= 0 */
-int power(int base, int n)
-{
-int i, p;
-p = 1;
-for (i = 1; i <= n; ++i)
-p = p * base;
-return p;
