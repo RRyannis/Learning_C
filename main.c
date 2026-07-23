0fed1a11681d43ed8ee30e0f0efed0ac325acde6
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-07-23 19:56:30 +0300

    functions

--- main.c
+++ main.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
+
+int power(int m, int n);
+
 int main() {
-    
-    char item[50] = "";
-    float price = 0.0f;
-    int quantity = 0;
-    char currency = '$';
-    float total = 0.0f;
-    
-    printf("What would you like to buy: ");
-    fgets(item, sizeof(item), stdin);
-    item[strlen(item) - 1] = 0;
-    printf("What is the price of the item: ");
-    scanf("%f", &price);
-    printf("How many would you like to buy: ");
-    scanf("%d", &quantity);
-
-    total = price * quantity;
-
-    printf("You have purchased %d %s for a total of %c%.2f\n", quantity, item, currency, total);
-
-
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("%d %d %d\n", i,power(2,i), power(-3,i));
+    }
     return 0;
 }
+
+int power(int base, int n) {
+    int i, p;
+    p = 1;
+    for (i = 1; i <= n; i++) {
+        p = p * base;
+    }
+    return p;
+}
+
+// int main() {
+    
+//     char item[50] = "";
+//     float price = 0.0f;
+//     int quantity = 0;
+//     char currency = '$';
+//     float total = 0.0f;
+    
+//     printf("What would you like to buy: ");
+//     fgets(item, sizeof(item), stdin);
+//     item[strlen(item) - 1] = 0;
+//     printf("What is the price of the item: ");
+//     scanf("%f", &price);
+//     printf("How many would you like to buy: ");
+//     scanf("%d", &quantity);
+
+//     total = price * quantity;
+
+//     printf("You have purchased %d %s for a total of %c%.2f\n", quantity, item, currency, total);
+
+
+//     return 0;
+// }
 
 
 
