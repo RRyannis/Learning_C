31d7fa6703684f56cb4efc3457217abfccb29c6d
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-07-24 13:05:10 +0300

    arrays

--- main.c
+++ main.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
-#include <string.h>
-
-
-int power(int m, int n);
-
-int main() {
-    int i;
-    for (i = 0; i < 10; i++) {
-        printf("%d %d %d\n", i,power(2,i), power(-3,i));
-    }
-    return 0;
+#define MAXLINE 1000 /* maximum input
+line length */
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+ /* print the longest input line */
+main()
+{
+int len; /* current line
+length */
+int max; /* maximum length
+seen so far */
+char line[MAXLINE]; /* current
+input line */
+char longest[MAXLINE]; /* longest
+line saved here */
+max = 0;
+while ((len = getline(line, MAXLINE))
+> 0)
+if (len > max) {
+max = len;
+copy(longest, line);
 }
-
-int power(int base, int n) {
-    int i, p;
-    p = 1;
-    for (i = 1; i <= n; i++) {
-        p = p * base;
-    }
-    return p;
+if (max > 0) /* there was a line */
+printf("%s", longest);
+return 0;
+}
+/* getline: read a line into s, return
+length */
+int getline(char s[],int lim)
+{
+int c, i;
+for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+s[i] = c;
+if (c == '\n') {
+ s[i] = c;
+++i;
+}
+s[i] = '\0';
+return i;
+}
+/* copy: copy 'from' into 'to'; assume
+to is big enough */
+void copy(char to[], char from[])
+{
+int i;
+i = 0;
+while ((to[i] = from[i]) != '\0')
+++i;
 }
 
 // int main() {
