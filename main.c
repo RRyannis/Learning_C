b14b81af5d29e9ab60346bea3e843e03e18f1399
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-07-25 23:50:08 +0300

    silly

--- main.c
+++ main.c
@@ -1,52 +1,30 @@
+/* count lines, words, and characters (like `wc`) */
 #include <stdio.h>
-#define MAXLINE 1000 /* maximum input
-line length */
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
- /* print the longest input line */
-main()
+
+#define IN   1  /* inside a word */
+#define OUT  0  /* outside a word */
+
+int main(void)
 {
-int len; /* current line
-length */
-int max; /* maximum length
-seen so far */
-char line[MAXLINE]; /* current
-input line */
-char longest[MAXLINE]; /* longest
-line saved here */
-max = 0;
-while ((len = getline(line, MAXLINE))
-> 0)
-if (len > max) {
-max = len;
-copy(longest, line);
-}
-if (max > 0) /* there was a line */
-printf("%s", longest);
-return 0;
-}
-/* getline: read a line into s, return
-length */
-int getline(char s[],int lim)
-{
-int c, i;
-for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-s[i] = c;
-if (c == '\n') {
- s[i] = c;
-++i;
-}
-s[i] = '\0';
-return i;
-}
-/* copy: copy 'from' into 'to'; assume
-to is big enough */
-void copy(char to[], char from[])
-{
-int i;
-i = 0;
-while ((to[i] = from[i]) != '\0')
-++i;
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("lines = %d, words = %d, chars = %d\n", nl, nw, nc);
+    return 0;
 }
 
 // int main() {
