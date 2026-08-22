f47f4dbe56a60b7377ff4a519e8d6fc07f700248
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-08-22 18:59:01 +0300

    eoppep

--- eoppep.c
+++ eoppep.c
@@ -103,48 +103,71 @@
 // }
 
 //Exam question 36, quick sort in C
+// #include <stdio.h>
+
+// void swap(int *a, int *b) {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+// int partition(int arr[], int low, int high) {
+//     int pivot = arr[high];
+//     int i = (low - 1);
+//     for (int j = low; j < high; j++) {
+//         if (arr[j] <= pivot) {
+//             i++;
+//             swap(&arr[i], &arr[j]);
+//         }
+//     }
+//     swap(&arr[i + 1], &arr[high]);
+//     return (i + 1);
+// }
+
+// void quickSort(int arr[], int low, int high) {
+//     if (low < high) {
+//         int partitionIndex = partition(arr, low, high);
+//         quickSort(arr, low, partitionIndex - 1);
+//         quickSort(arr, partitionIndex + 1, high);
+//     }
+// }
+
+// void printArray(int arr[], int size) {
+//     for (int i = 0; i < size; i++) {
+//         printf("%d", arr[i]);
+//         printf("\n");
+//     }
+// }
+
+// int main(void) {
+//     int arr[] = {10, 7, 8, 9, 1, 5};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     printf("Initial array: \n");
+//     printArray(arr, n);
+//     quickSort(arr, 0, n-1);
+//     printf("Sorted array: \n");
+//     printArray(arr, n);
+//     return 0;
+// }
+
+
+//Exam question 10, groub B
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
+int main (void) {
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) {
+            break;        
         }
+        printf("%d", i);
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
+    printf("\n");
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int partitionIndex = partition(arr, low, high);
-        quickSort(arr, low, partitionIndex - 1);
-        quickSort(arr, partitionIndex + 1, high);
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) {
+            continue;
+        }
+        printf("%d", i);
     }
-}
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d", arr[i]);
-        printf("\n");
-    }
-}
-
-int main(void) {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Initial array: \n");
-    printArray(arr, n);
-    quickSort(arr, 0, n-1);
-    printf("Sorted array: \n");
-    printArray(arr, n);
     return 0;
 }
