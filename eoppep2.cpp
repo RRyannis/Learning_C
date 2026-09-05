9d80414d644e81c4a8b2381bcd59c69d1134d95a
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-05 13:19:40 +0300

    eoppep cpp

--- eoppep2.cpp
+++ eoppep2.cpp
@@ -9,26 +9,55 @@
 
 //εοππεπ ερώτηση 162, ομάδα Α
 
+// #include <iostream>
+
+// using namespace std;
+
+// int factorial(int n) {
+//     if (n == 0) {
+//         return 1;
+//     } else {
+//         return n*factorial(n-1);
+//     }
+// }
+
+// int main(void) {
+//     int number;
+//     cout<<"Enter a number:";
+//     cin>>number;
+//     if (number < 0) {
+//         cout<<"Has to be a positive integer."<<endl;
+//     } else {
+//         cout<<"The factorial of "<<number<<" is "<<factorial(number)<<endl;
+//     }
+//     return 0;
+// }
+
+//εοππεπ ερώτηση 163, ομάδα Α
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n*factorial(n-1);
-    }
-}
+int main() {
+    
+    string sentence = "C++ is an object oriented programming language and is a superset of C.";
+    string wordToFind = "is";
+    int position = 1;
+    int count = 0;
 
-int main(void) {
-    int number;
-    cout<<"Enter a number:";
-    cin>>number;
-    if (number < 0) {
-        cout<<"Has to be a positive integer."<<endl;
+    size_t found = sentence.find(wordToFind);
+    while (found != string::npos) {
+        count++;
+        position = found;
+        found = sentence.find(wordToFind, found + 1);
+    }
+
+    if (count > 0) {
+        cout<<"The word "<< wordToFind << " has been found "<< count << " times."<<endl;
+        cout<<"Its last position is "<< position << endl;
     } else {
-        cout<<"The factorial of "<<number<<" is "<<factorial(number)<<endl;
+        cout<<"The word "<< wordToFind<<" wasn't found"<<endl; 
     }
     return 0;
 }
