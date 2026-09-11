932778c2d42a8dd1c2a43268131cd617f97417a2
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-11 18:09:16 +0300

    c bubblesort

--- eoppep.c
+++ eoppep.c
@@ -384,48 +384,79 @@
 // }
 
 
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// struct TreeNode {
+//     int value;
+//     struct TreeNode *left;
+//     struct TreeNode *right;
+// };
+
+// struct TreeNode *insert(struct TreeNode *root, int value)
+// {
+//     if (root == NULL) {
+//         struct TreeNode *node = malloc(sizeof(struct TreeNode));
+//         node->value = value;
+//         node->left = node->right = NULL;
+//         return node;
+//     }
+//     if (value < root->value)
+//         root->left = insert(root->left, value);
+//     else
+//         root->right = insert(root->right, value);
+//     return root;
+// }
+
+// void inorder(struct TreeNode *root)
+// {
+//     if (root == NULL)
+//         return;
+//     inorder(root->left);
+//     printf("%d ", root->value);
+//     inorder(root->right);
+// }
+
+// int main(void)
+// {
+//     struct TreeNode *root = NULL;
+//     int values[] = {5, 3, 8, 1, 4, 7, 9};
+
+//     for (int i = 0; i < 7; i++)
+//         root = insert(root, values[i]);
+
+//     inorder(root);   /* prints sorted: 1 3 4 5 7 8 9 */
+//     printf("\n");
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
-#include <stdlib.h>
 
-struct TreeNode {
-    int value;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
-
-struct TreeNode *insert(struct TreeNode *root, int value)
+void bubble_sort(int arr[], int n)
 {
-    if (root == NULL) {
-        struct TreeNode *node = malloc(sizeof(struct TreeNode));
-        node->value = value;
-        node->left = node->right = NULL;
-        return node;
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-    if (value < root->value)
-        root->left = insert(root->left, value);
-    else
-        root->right = insert(root->right, value);
-    return root;
-}
-
-void inorder(struct TreeNode *root)
-{
-    if (root == NULL)
-        return;
-    inorder(root->left);
-    printf("%d ", root->value);
-    inorder(root->right);
 }
 
 int main(void)
 {
-    struct TreeNode *root = NULL;
-    int values[] = {5, 3, 8, 1, 4, 7, 9};
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = 6;
 
-    for (int i = 0; i < 7; i++)
-        root = insert(root, values[i]);
+    bubble_sort(arr, n);
 
-    inorder(root);   /* prints sorted: 1 3 4 5 7 8 9 */
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     printf("\n");
 
     return 0;
