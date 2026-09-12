3acbdd14bdc22ea506035382513ab8d153e41241
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-12 20:05:43 +0300

    dsa c

--- eoppep.c
+++ eoppep.c
@@ -432,32 +432,61 @@
 // }
 
 
+// #include <stdio.h>
+
+// void bubble_sort(int arr[], int n)
+// {
+//     int i, j, temp;
+//     for (i = 0; i < n - 1; i++) {
+//         for (j = 0; j < n - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
+// int main(void)
+// {
+//     int arr[] = {5, 2, 9, 1, 5, 6};
+//     int n = 6;
+
+//     bubble_sort(arr, n);
+
+//     for (int i = 0; i < n; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-void bubble_sort(int arr[], int n)
+int binary_search(int arr[], int n, int target)
 {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
     }
+    return -1;   /* not found */
 }
 
 int main(void)
 {
-    int arr[] = {5, 2, 9, 1, 5, 6};
+    int arr[] = {1, 3, 5, 7, 9, 11};
     int n = 6;
 
-    bubble_sort(arr, n);
-
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    printf("index: %d\n", binary_search(arr, n, 7));   /* 3 */
+    printf("index: %d\n", binary_search(arr, n, 4));   /* -1 */
 
     return 0;
 }
