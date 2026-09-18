b4ce18fe031983756789b3cdb17b052979641774
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-18 16:10:46 +0300

    dsa c

--- data_structures.c
+++ data_structures.c
@@ -1,46 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct TreeNode {
-    int value;
-    struct TreeNode *left;
-    struct TreeNode *right;
+// struct TreeNode {
+//     int value;
+//     struct TreeNode *left;
+//     struct TreeNode *right;
+// };
+
+// struct TreeNode *insert(struct TreeNode *root, int value)
+// {
+//     if (root == NULL) {
+//         struct TreeNode *node = malloc(sizeof(struct TreeNode));
+//         node->value = value;
+//         node->left = node->right = NULL;
+//         return node;
+//     }
+//     if (value < root->value)
+//         root->left = insert(root->left, value);
+//     else
+//         root->right = insert(root->right, value);
+//     return root;
+// }
+
+// void inorder(struct TreeNode *root)
+// {
+//     if (root == NULL)
+//         return;
+//     inorder(root->left);
+//     printf("%d ", root->value);
+//     inorder(root->right);
+// }
+
+// int main(void)
+// {
+//     struct TreeNode *root = NULL;
+//     int values[] = {5, 3, 8, 1, 4, 7, 9};
+
+//     for (int i = 0; i < 7; i++)
+//         root = insert(root, values[i]);
+
+//     inorder(root);   /* prints sorted: 1 3 4 5 7 8 9 */
+//     printf("\n");
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
 };
 
-struct TreeNode *insert(struct TreeNode *root, int value)
+struct Node *push(struct Node *head, int value)
 {
-    if (root == NULL) {
-        struct TreeNode *node = malloc(sizeof(struct TreeNode));
-        node->value = value;
-        node->left = node->right = NULL;
-        return node;
-    }
-    if (value < root->value)
-        root->left = insert(root->left, value);
-    else
-        root->right = insert(root->right, value);
-    return root;
+    struct Node *new_node = malloc(sizeof(struct Node));
+    new_node->data = value;
+    new_node->next = head;   
+    return new_node;        
 }
 
-void inorder(struct TreeNode *root)
+void print_list(struct Node *head)
 {
-    if (root == NULL)
-        return;
-    inorder(root->left);
-    printf("%d ", root->value);
-    inorder(root->right);
+    struct Node *current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void free_list(struct Node *head)
+{
+    struct Node *tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
 }
 
 int main(void)
 {
-    struct TreeNode *root = NULL;
-    int values[] = {5, 3, 8, 1, 4, 7, 9};
+    struct Node *head = NULL;
 
-    for (int i = 0; i < 7; i++)
-        root = insert(root, values[i]);
+    head = push(head, 3);
+    head = push(head, 2);
+    head = push(head, 1);
 
-    inorder(root);   /* prints sorted: 1 3 4 5 7 8 9 */
-    printf("\n");
+    print_list(head);  
+
+    free_list(head);
 
     return 0;
 }
