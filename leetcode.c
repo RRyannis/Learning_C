fdef92ca6f5b64996aa235ff2db70c5591a49339
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-20 10:36:34 +0300

    leetcode c

--- leetcode.c
+++ leetcode.c
@@ -1,25 +1,45 @@
-//problem 58 length of last string
+//problem 136 single number
 #include <stdio.h>
-#include <string.h>
-
-int lengthOfLastWord(char* s) {
-    int length = 0;
-    for (int i = strlen(s) - 1; i >=0; i--){
-        if(s[i] == ' ' && length == 0){
-            continue;
-        } else if (s[i] == ' ' && length > 0) {
-            return length;
-        } else {
-            length++;
-        }
+int singleNumber(int* nums, int numsSize){
+    int result = 0;
+    for (int i = 0; i <= numsSize-1; i++) {
+        result ^= nums[i];
+        printf("%d \n", result);
     }
-    return length;
+    return result;
 }
 
 int main(void) {
-    char stringg[] = "Hello World  ";
-    int size = lengthOfLastWord(stringg);
-    printf("%d", size);
+    int nums[] = {4, 1, 2, 1, 2};
+    int lengthOfNums = sizeof(nums)/sizeof(nums[0]);
+    printf("%d \n", singleNumber(nums, lengthOfNums));
+    
     
     return 0;
 }
+
+//problem 58 length of last string
+// #include <stdio.h>
+// #include <string.h>
+
+// int lengthOfLastWord(char* s) {
+//     int length = 0;
+//     for (int i = strlen(s) - 1; i >=0; i--){
+//         if(s[i] == ' ' && length == 0){
+//             continue;
+//         } else if (s[i] == ' ' && length > 0) {
+//             return length;
+//         } else {
+//             length++;
+//         }
+//     }
+//     return length;
+// }
+
+// int main(void) {
+//     char stringg[] = "Hello World  ";
+//     int size = lengthOfLastWord(stringg);
+//     printf("%d", size);
+    
+//     return 0;
+// }
