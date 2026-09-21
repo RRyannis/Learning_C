8cfa9bd07301c2aef90bf5b611a6b9e268ebecc4
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-21 12:31:17 +0300

    leetcode 283

--- leetcode.c
+++ leetcode.c
@@ -1,22 +1,49 @@
-//problem 136 single number
+//problem 283 single move zeroes
 #include <stdio.h>
-int singleNumber(int* nums, int numsSize){
-    int result = 0;
-    for (int i = 0; i <= numsSize-1; i++) {
-        result ^= nums[i];
-        printf("%d \n", result);
-    }
-    return result;
-}
+    
+void moveZeroes(int* nums, int numsSize) {
 
+    int writeIndex = 0;
+    for (int readIndex = 0; readIndex < numsSize; readIndex++) {
+        if (nums[readIndex] != 0) {
+            nums[writeIndex] = nums[readIndex];
+            writeIndex++;
+        }
+    }
+    for (int i = writeIndex; i < numsSize; i ++) {
+        nums[i] = 0;
+    }
+
+}
 int main(void) {
-    int nums[] = {4, 1, 2, 1, 2};
-    int lengthOfNums = sizeof(nums)/sizeof(nums[0]);
-    printf("%d \n", singleNumber(nums, lengthOfNums));
-    
-    
+    int nums[] = {0,4,6,2,0,4,0};
+    int size = sizeof(nums)/sizeof(nums[0]);
+    moveZeroes(nums, size);
+    for (int i = 0; i < size; i++) {
+        printf("%d ", nums[i]);
+    }
     return 0;
 }
+
+// //problem 136 single number
+// #include <stdio.h>
+// int singleNumber(int* nums, int numsSize){
+//     int result = 0;
+//     for (int i = 0; i <= numsSize-1; i++) {
+//         result ^= nums[i];
+//         printf("%d \n", result);
+//     }
+//     return result;
+// }
+
+// int main(void) {
+//     int nums[] = {4, 1, 2, 1, 2};
+//     int lengthOfNums = sizeof(nums)/sizeof(nums[0]);
+//     printf("%d \n", singleNumber(nums, lengthOfNums));
+    
+    
+//     return 0;
+// }
 
 //problem 58 length of last string
 // #include <stdio.h>
