94b5dbfa0dc79d13650923c099dfa792fbab6c38
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-09-26 10:59:07 +0300

    dsa leetcode problem 69

--- leetcode.c
+++ leetcode.c
@@ -1,31 +1,62 @@
-//problem 704 binary search
+// problem 69 sqrt(x)
 #include <stdio.h>
-int search(int* nums, int numsSize, int target) {
+int mySqrt(int x) {
+    int mid = 0;
     int low = 0;
-    int high = numsSize - 1;
-    while(low <= high){
-        int mid = low + (high - low) / 2;
+    int high = x;
+    int answer = 0;
+    while(low <= high) {
+        mid = low + (high - low) / 2;
 
-        if (nums[mid] == target) {
-            return mid;
-        }
-        if ( nums[mid] > target){
-            high = mid - 1;
-        } else {
+        if (mid * mid <= x) {
+            
+            answer = mid;
             low = mid + 1;
+
+        } else {
+            high = mid -1;
         }
     }
-    return -1;
+    return answer;
 }
+
 int main(void) {
-    int arr[] = { 1, 3, 5, 6 };
-    int x = 3;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int result = search(arr, n, x);
-    printf("%d \n",result);
+    int x = 8;
+    int result = 0;
+    result = mySqrt(x);
+    printf("%d \n", result);
 
     return 0;
 }
+
+//problem 704 binary search
+// #include <stdio.h>
+// int search(int* nums, int numsSize, int target) {
+//     int low = 0;
+//     int high = numsSize - 1;
+//     while(low <= high){
+//         int mid = low + (high - low) / 2;
+
+//         if (nums[mid] == target) {
+//             return mid;
+//         }
+//         if ( nums[mid] > target){
+//             high = mid - 1;
+//         } else {
+//             low = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+// int main(void) {
+//     int arr[] = { 1, 3, 5, 6 };
+//     int x = 3;
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     int result = search(arr, n, x);
+//     printf("%d \n",result);
+
+//     return 0;
+// }
 //problem 35 search insert position
 // #include <stdio.h>
 // int searchInsert(int* nums, int numsSize, int target) {
